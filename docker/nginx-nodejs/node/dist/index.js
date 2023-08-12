9528a1b14328a726a0990af631039f80a9483283
Eduardo Paiva <eduardocspv@gmail.com>
2023-08-11 22:20:05 -0300

    FC-001 - docker-compose wait for it

--- docker/nginx-nodejs/node/dist/index.js
+++ docker/nginx-nodejs/node/dist/index.js
@@ -5,11 +5,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mysql_1 = __importDefault(require("mysql"));
+const chance_1 = __importDefault(require("chance"));
 dotenv_1.default.config();
+const CHANCE = new chance_1.default();
+const config = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+};
+const connection = mysql_1.default.createConnection(config);
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const insertRandomName = () => {
+    const name = CHANCE.name();
+    connection.query(`INSERT INTO people (name) VALUES ('${name}')`);
+};
 app.get('/', (req, res) => {
-    res.send(`<h1>Full Cycle Rocks!</h1>`);
+    insertRandomName();
+    connection.query(`SELECT name FROM people`, (error, results, fields) => {
+        res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map((el) => `<li>${el.name}</li>`).join('') : ''}
+      </ol>
+    `);
+    });
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
