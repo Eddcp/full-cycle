572f8a6e673a5df2ccf59adefb730b62c83f6812
Eduardo Paiva <eduardocspv@gmail.com>
2023-09-22 10:16:41 -0300

    FC-OBS-002 - Adding new metrics to go app

--- observability/prometheus/main.go
+++ observability/prometheus/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -17,15 +18,51 @@ var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var totalRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "goapp_total_requests",
+	Help: "Total requests",
+}, []string{})
+
+var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "goapp_http_request_duration",
+	Help: "Duration in seconds of all HTTP requests",
+}, []string{"handler"})
+
 func main() {
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlineUsers)
+	r.MustRegister(totalRequests)
+	r.MustRegister(httpDuration)
 
 	go func() {
 		for {
 			onlineUsers.Set(float64(rand.Intn(2000)))
 		}
 	}()
+
+	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome"))
+	})
+
+	contact := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration(rand.Intn(5))*time.Second)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Contact"))
+	})
+
+	firstDuration := promhttp.InstrumentHandlerDuration(
+		httpDuration.MustCurryWith(prometheus.Labels{"handler": "home"}),
+		promhttp.InstrumentHandlerCounter(totalRequests, home),
+	)
+
+	secondDuration := promhttp.InstrumentHandlerDuration(
+		httpDuration.MustCurryWith(prometheus.Labels{"handler": "contact"}),
+		promhttp.InstrumentHandlerCounter(totalRequests, contact),
+	)
+
+	http.Handle("/", firstDuration)
+	http.Handle("/contact", secondDuration)
 
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	log.Fatal(http.ListenAndServe(":8181", nil))
