2d4a559663848999aa4c741f6e29cbb27d6aa3ee
Eduardo Paiva <eduardocspv@gmail.com>
2023-10-11 09:56:37 -0300

    FC-DDD-001 - Adding first entities and simple test

---
+++ ddd-patterns/src/main.ts
@@ -0,0 +1,12 @@
+import Customer from "./entity/customer";
+import Order from "./entity/order";
+import OrderItem from "./entity/order-item";
+import Address from "./value-object/address";
+
+let customer = new Customer("123", "John Doe");
+const address = new Address("1st Street", 2, "12345-78", "Some place");
+customer.Address = address;
+customer.activate();
+
+const item = new OrderItem("1", "Item 1", 10);
+const order = new Order("1", "123", [item]);
