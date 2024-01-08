49d59cb369dcca5a495ee2e723f73ed1914cc954
Eduardo Paiva <eduardocspv@gmail.com>
2024-01-08 16:35:12 -0300

    FC-000 - Adding db adapter

---
+++ hexagonal-architecture/adapters/db/product_test.go
@@ -0,0 +1,54 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/eddcp/full-cycle/hexagonal-architecture-go/adapters/db"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setup() {
+	Db, _ = sql.Open("sqlite3", ":memory:") 
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+		"id" string,
+		"name" string,
+		"price" float,
+		"status" string
+	);`
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO products values("abc", "Product Test", 0, "disabled")`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setup()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+}
