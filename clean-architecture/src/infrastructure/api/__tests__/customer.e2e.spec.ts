c03a1aa96cc30cc93fd3b93612854c6151dc2289
Eduardo Paiva <eduardocspv@gmail.com>
2024-04-22 20:44:02 -0300

    FC-000 - Adding presenters

--- clean-architecture/src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ clean-architecture/src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -67,12 +67,24 @@ describe("E2E test for customer API", () => {
     const listResponse = await request(app).get("/customer").send();
 
     expect(listResponse.status).toBe(200);
-    // expect(listResponse.body.customers.length).toBe(2);
-    // const customer = listResponse.body.customers[0];
-    // expect(customer.name).toBe("John");
-    // expect(customer.address.street).toBe("Street");
-    // const customer2 = listResponse.body.customers[1];
-    // expect(customer2.name).toBe("Jane");
-    // expect(customer2.address.street).toBe("Street 2");
+    expect(listResponse.body.customers.length).toBe(2);
+    const customer = listResponse.body.customers[0];
+    expect(customer.name).toBe("John");
+    expect(customer.address.street).toBe("Street");
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Jane");
+    expect(customer2.address.street).toBe("Street 2");
+
+    const listResponseXML = await request(app)
+      .get("/customer")
+      .set("Accept", "application/xml")
+      .send();
+
+    expect(listResponseXML.status).toBe(200);
+    expect(listResponseXML.text).toContain(
+      `<?xml version="1.0" encoding="UTF-8"?>`
+    );
+    expect(listResponseXML.text).toContain(`<customers>`);
+    expect(listResponseXML.text).toContain(`<name>Jane</name>`);
   });
 });
